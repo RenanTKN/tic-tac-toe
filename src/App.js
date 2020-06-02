1c9d468b5eaf2ddacfa3d2e274964795bae05f9b
RenanTKN <renanwds@gmail.com>
2020-06-01 22:49:36 -0300

    Convert to hooks

---
+++ src/App.js
@@ -0,0 +1,207 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const Square = ({ highlight, value, onClick }) => {
+  const className =
+    "square" + (highlight ? " highlight" : "") + (value === "X" ? " x" : " o");
+  return (
+    <button className={className} onClick={onClick}>
+      <div>{value}</div>
+    </button>
+  );
+};
+
+const Board = (props) => {
+  const renderSquare = (i) => {
+    return (
+      <Square
+        key={i}
+        value={props.squares[i]}
+        onClick={() => props.onClick(i)}
+        highlight={props.winLine && props.winLine.includes(i)}
+      />
+    );
+  };
+
+  const boardSize = 3;
+  let squares = [];
+  for (let i = 0; i < boardSize; i++) {
+    let row = [];
+    for (let j = 0; j < boardSize; j++) {
+      row.push(renderSquare(i * boardSize + j));
+    }
+    squares.push(
+      <div key={i} className="board-row">
+        {row}
+      </div>
+    );
+  }
+  return <div>{squares}</div>;
+};
+
+export default () => {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [isAscending, setIsAscending] = useState(true);
+  const [xPoints, setXPoints] = useState(0);
+  const [oPoints, setOPoints] = useState(0);
+
+  const handleClick = (i) => {
+    const hst = history.slice(0, stepNumber + 1);
+
+    const current = hst[hst.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares).winner || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    const winner = calculateWinner(squares).winner;
+    updateScore(winner);
+    setHistory(
+      hst.concat([
+        {
+          squares: squares,
+          lastMove: i,
+        },
+      ])
+    );
+    setStepNumber(hst.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const handleSortToggle = () => {
+    setIsAscending(!isAscending);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(!(step % 2));
+  };
+
+  const updateScore = (winner) => {
+    if (winner === "X") {
+      setXPoints(xPoints + 1);
+    } else if (winner === "O") {
+      setOPoints(oPoints + 1);
+    }
+  };
+
+  const current = history[stepNumber];
+  const info = calculateWinner(current.squares);
+  const winner = info.winner;
+  const line = info.line;
+  const isDraw = info.isDraw;
+
+  const moves = history.map((step, move) => {
+    const lastMove = step.lastMove;
+    const col = (lastMove % 3) + 1,
+      row = Math.floor(lastMove / 3) + 1;
+    const desc = move
+      ? "Go to move #" + move + " (" + col + ", " + row + ")"
+      : "Go to game start";
+    return (
+      <li key={move}>
+        <button
+          className={move === stepNumber ? "currently-selected-item" : ""}
+          onClick={() => jumpTo(move)}
+        >
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
+  if (!isAscending) {
+    moves.reverse();
+  }
+
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else if (isDraw) {
+    status = "Draw";
+  } else {
+    status = "Next player : " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board-container">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => handleClick(i)}
+            winLine={line}
+          />
+        </div>
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+        <div className="scoreboard-container">
+          <table className="scoreboard">
+            <thead>
+              <tr>
+                <th className="x">X</th>
+                <th className="o">O</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{xPoints}</td>
+                <td>{oPoints}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <button className="order-button" onClick={() => handleSortToggle()}>
+          ⇅ {isAscending ? "descending" : "ascending"}
+        </button>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  let isVictory = false;
+  let allLines = [];
+  let winner;
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      winner = squares[a];
+      Array.prototype.push.apply(allLines, lines[i]);
+      isVictory = true;
+    }
+  }
+  if (isVictory) {
+    return {
+      winner: winner,
+      line: allLines,
+      isDraw: false,
+    };
+  }
+
+  let isDraw = true;
+  if (squares.filter((x) => x === null).length !== 0) {
+    isDraw = false;
+  }
+
+  return {
+    winner: null,
+    line: null,
+    isDraw: isDraw,
+  };
+};
